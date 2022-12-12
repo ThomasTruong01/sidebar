b43337cdf7d7c2e854d03d481432e960183ac7b6
Thomas Truong <thomas.truong01@gmail.com>
2022-12-12 01:11:54 -0800

    Toggle Dark Mode

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,43 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import SideBar from '../components/SideBar/Sidebar'
+import Switch from "react-switch"
+import { useState, useEffect } from 'react'
+
 
 export default function Home() {
+  const [DarkMode, setDarkMode] = useState(false)
+
+  const handleChange = () => {
+    setDarkMode(!DarkMode);
+
+    toggleDarkMode(DarkMode);
+    setDarkModeLocalStorage(DarkMode);
+  }
+
+
+// MediaQueryList object
+
+// Toggles the "dark-mode" class
+const toggleDarkMode = (state) => {
+  document.querySelector(`body`).setAttribute('dark-mode',state);
+}
+
+// Sets localStorage state
+const setDarkModeLocalStorage = (state) => {
+  localStorage.setItem("dark-mode", state);
+}
+
+useEffect(() => {
+  const useDark = window.matchMedia("(prefers-color-scheme: dark)");
+}, [])
+
+useEffect(() => {
+  const useDark = window.matchMedia("(prefers-color-scheme: dark)");
+  let darkModeState = useDark.matches;
+  toggleDarkMode(DarkMode);
+}, [DarkMode])
+
   return (
     <div className={styles.container}>
       <Head>
@@ -15,6 +49,26 @@ export default function Home() {
 
       <SideBar />
       <main className={styles.main}>
+        <div className={styles.card}>
+          <p className={styles.title}>Switch Between Light and Dark Mode</p>
+          {/* <Switch onChange={handleChange} checked={DarkMode} /> */}
+          <Switch
+                checked={DarkMode}
+                onChange={handleChange}
+                offColor="#1416b8"
+                onColor="#353535"
+                // className="react-switch mx-auto"
+                width={90}
+                height={40}
+                uncheckedIcon={
+                  <i className='bx bx-moon' ></i>
+                }
+                checkedIcon={
+                  <i className='bx bxs-sun' ></i>
+                }
+                id="icon-switch"
+              />
+        </div>
       </main>
     </div>
   )
